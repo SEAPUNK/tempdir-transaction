3cc7aa0b0d8c969968039bb228f848fb2f586908
Ivan <ivan@sq10.net>
2016-06-20 10:03:51 -0500

    fix code

--- index.js
+++ index.js
@@ -5,20 +5,17 @@ var mkdirp = require('mkdirp')
 var rimraf = require('rimraf')
 
 module.exports = function (fn) {
-  var self = this
-  return new Promise(function (resolve, reject) { 
-    if (typeof fn !== 'function') {
-      return reject(new Error('Argument must be a function')
-      var tempDir = uniqueTempDir()
-      mkdirp(tempDir, function (err) {
-        if (err) return reject(err)
-        Promise.resolve(fn(tempDir)).then(function (retval) {
-          rimraf(tempDir, function (err) {
-            if (err) return reject(err)
-            return resolve(retval)
-          })
-        }).catch(reject)
-      })
-    }
+  return new Promise(function (resolve, reject) {
+    if (typeof fn !== 'function') return reject(new Error('Argument must be a function'))
+    var tempDir = uniqueTempDir()
+    mkdirp(tempDir, function (err) {
+      if (err) return reject(err)
+      Promise.resolve(fn(tempDir)).then(function (retval) {
+        rimraf(tempDir, function (err) {
+          if (err) return reject(err)
+          return resolve(retval)
+        })
+      }).catch(reject)
+    })
   })
 }
