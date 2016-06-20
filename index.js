47868924d68c01ea834d4ef4c9c1f1f32bd6a169
Ivan <ivan@sq10.net>
2016-06-20 10:01:46 -0500

    code

---
+++ index.js
@@ -0,0 +1,24 @@
+'use strict'
+
+var uniqueTempDir = require('unique-temp-dir')
+var mkdirp = require('mkdirp')
+var rimraf = require('rimraf')
+
+module.exports = function (fn) {
+  var self = this
+  return new Promise(function (resolve, reject) { 
+    if (typeof fn !== 'function') {
+      return reject(new Error('Argument must be a function')
+      var tempDir = uniqueTempDir()
+      mkdirp(tempDir, function (err) {
+        if (err) return reject(err)
+        Promise.resolve(fn(tempDir)).then(function (retval) {
+          rimraf(tempDir, function (err) {
+            if (err) return reject(err)
+            return resolve(retval)
+          })
+        }).catch(reject)
+      })
+    }
+  })
+}
